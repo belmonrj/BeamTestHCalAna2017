f1101df31476a0b6ef0eaea7f9ea2f167c4f1811
Ron Belmont <belmonrj@gmail.com>
2017-10-01 22:27:27 -0400

    Name change and new figure locations

--- Resolution_generic_FullCalo.C
+++ Resolution_generic_FullCalo.C
@@ -1,6 +1,6 @@
 
 
-void Resolution_generic()
+void Resolution_generic_FullCalo()
 {
 
   // --- energy values
@@ -148,7 +148,7 @@ void Resolution_generic()
       linearity->SetBinContent(linearity->FindBin(en[ifile]), gaus->GetParameter(1));
       linearity->SetBinError(linearity->FindBin(en[ifile]), gaus->GetParError(1));
     }
-  can->Print("figure_multipanel_energyreso.png");
+  can->Print("figures/RB_fullcalo_multipanel_energyreso.png");
   // --- setup for more plots, not printed to file
   TLegend * legend = new TLegend( 0.2, 0.7, 0.8, 0.88, "", "NDC" );
   legend->SetFillColor(0);
@@ -170,12 +170,12 @@ void Resolution_generic()
   legend->AddEntry( f_calo_r,  name.Data(), "L" );
   legend->Draw();
   can2->Update();
-  can2->Print("figure_energyreso.png");
+  can2->Print("figures/RB_fullcalo_energyreso.png");
   // --- linearity plot starts here
   TCanvas *c2 = new TCanvas("c2","c2",800,600);
   linearity->Draw();
   linearity->Fit("pol1");
-  c2->Print("figure_energylinearity.png");
+  c2->Print("figures/RB_fullcalo_energylinearity.png");
   // --- now write all the histograms to file
   fout->cd();
   res->Write();
